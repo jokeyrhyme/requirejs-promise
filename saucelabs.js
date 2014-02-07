d8e929c5e731fd88ade73e7ff3fb29d422fbaa3c
Ron Waldon <jokeyrhyme@gmail.com>
2014-02-07 22:30:58 +1100

    travis-ci: configure saucelabs to test with more browsers

--- saucelabs.js
+++ saucelabs.js
@@ -14,8 +14,41 @@ module.exports = {
   browsers: [
     {
       browserName: "internet explorer",
-      platform: "WIN8",
-      version: "10"
+      version: "11",
+      platform: "Windows 8.1"
+    },
+    {
+      browserName: "internet explorer",
+      version: "10",
+      platform: "Windows 8"
+    },
+    {
+      browserName: "firefox",
+      version: "26",
+      platform: "Windows 7"
+    },
+    {
+      browserName: "googlechrome",
+      version: "31",
+      platform: "OS X 10.9"
+    },
+    {
+      browserName: "android",
+      version: "4.0",
+      platform: "Linux",
+      "device-orientation": "portrait"
+    },
+    {
+      browserName: "iphone",
+      version: "7",
+      platform: "OS X 10.9",
+      "device-orientation": "portrait"
+    },
+    {
+      browserName: "iphone",
+      version: "6.1",
+      platform: "OS X 10.8",
+      "device-orientation": "portrait"
     }
   ],
   testname: 'requirejs-promise',
